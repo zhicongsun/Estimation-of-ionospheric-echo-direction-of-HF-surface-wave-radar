12b183056e572b93e0e940beeb80c90324f1c112
RadarSun <hitsunzhicong@163.com>
2020-03-12 14:25:28 +0800

    解决mesh带来的XY颠倒的问题

--- beamforming.m
+++ beamforming.m
@@ -3,7 +3,7 @@ function beamforming(input)
     Function description:
             数字波束形成算法选择器
     Syntax：
-    Long description：
+    Log description：
             2020.03.11 加入normaldbf
 %}
     if nargin<1
@@ -22,17 +22,17 @@ function normaldbf()
     Function description:
             普通DBF算法，用于2D-DOA估计
     Syntax：
-    Long description：
-            2020.03.11 初步实现DBF，单个目标波信合理，多目标出现额外波峰
+    Log description：
+            2020.03.11 初步实现DBF，单个目标波信合理，多目标出现额外波峰，三维图XY颠倒解决,是mesh的问题
 %}
     global g_signal;
     global g_array;
     global g_echos;
     global g_para;
     
-    theta.rad=linspace(-pi/2,pi/2,201);
+    theta.rad=linspace(-pi/2,pi/2,181);
     theta.num = theta.rad/pi*180;
-    phi.rad=linspace(-pi/2,pi/2,201);
+    phi.rad=linspace(-pi/2,pi/2,181);
     phi.num = phi.rad/pi*180;
 
     wx=exp(-j*2*pi/g_signal.lamda*g_array.x_pos.'*cos(g_echos.theta.rad).*cos(g_echos.phi.rad));
@@ -48,37 +48,33 @@ function normaldbf()
             P(k,i) = W'*A;
         end        
     end
+    size(P)
     patternmag=abs(P);
     patternmagnorm=patternmag/max(max(patternmag));
     patterndB=20*log10(patternmag);
     patterndBnorm=20*log10(patternmagnorm);
-    figure();
-    %subplot(211);
-    h = mesh(theta.num(101:201),phi.num(101:201),patternmag(101:201,101:201));
+    
+    absp_theta = abs(P(:,(g_echos.phi.num+91)));
+    absp_phi = abs(P((g_echos.theta.num+91),:));
+    
+    figure('Name','方向图','NumberTitle','off','Color','white','Position',[600 50 550 750]);
+    subplot(311);
+    patternmag = patternmag';% mesh太坑了
+    h = meshc(phi.num(91:181),theta.num(91:181),patternmag(91:181,91:181));% mesh(X,Y,Z)X,Y分别对应Z的列、行
+    title('二维方向图');
     set(h,'Linewidth',2);
-    xlabel('俯仰(degree)');
-    ylabel('方位(degree)');
-    zlabel('magnitude(dB)');
-%     subplot(212);
-%     mesh(theta.num(101:201),phi.num(101:201),patterndBnorm(101:201,101:201));
-%     set(h,'Linewidth',2);
-%     xlabel('俯仰(degree)');
-%     ylabel('方位(degree)');
-%     zlabel('magnitude(dB)');
-
-
-%     subplot(211);
-%     plot(phi.rad*180/pi,patternmag);
-%     grid on;
-%     xlabel('phi.rad/degree')
-%     ylabel('amplitude/dB')
-%     title(['phi方向图','来波方向为' num2str(g_echos.phi.rad.rad*180/pi) '度']);
-%     subplot(212);
-%     plot(phi.rad,patterndBnorm,'r');
-%     grid on;
-%     xlabel('phi.rad/radian')
-%     ylabel('amplitude/dB')
-%     title(['phi方向图','来波方向为' num2str(g_echos.phi.rad.rad*180/pi) '度']);
-%     axis([-1.5 1.5 -50 0]);
-
+    xlabel('方位角phi(degree)');
+    ylabel('俯仰角theta(degree)');
+    zlabel('abs of P');
+    subplot(312);
+    plot(theta.num(91:181),absp_theta(91:181));
+    title('俯仰角 的方向图');
+    xlabel('theta(degree)');
+    ylabel('abs of Ptheta');
+    subplot(313);
+    plot(phi.num(91:181),absp_phi(91:181));
+    title('方位角 的方向图');
+    xlabel('phi(degree)');
+    ylabel('abs of Pphi');
+    
 end 
